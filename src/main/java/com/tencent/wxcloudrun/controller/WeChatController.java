19532ecd12a6fa73c63b6e8d4a3d6ce38d4f637d
Gan Luanqing <lantin1102@gmail.com>
2022-09-01 16:16:12 +0800

    feat:服务器确认 2 fix

--- src/main/java/com/tencent/wxcloudrun/controller/WeChatController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/WeChatController.java
@@ -7,12 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 import java.util.TreeSet;
 
-@Controller("/wechat")
+@Controller
+@RequestMapping("/wechat")
 @Slf4j
 public class WeChatController {
 
